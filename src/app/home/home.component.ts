86225fa8f082705e0222cfbd6537b604738adb99
fcarrion001 <fpc1118@gmail.com>
2018-04-27 14:06:04 -0400

    Created a data service and used it
    Created data service and created methods within it.
    Gave DataService access to home and about components.
    Added goals to about view.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, style, transition, animate, query, stagger, keyframes } from '@angular/animations';
+import { DataService } from '../data.service'
 
 @Component({
   selector: 'app-home',
@@ -32,21 +33,25 @@ export class HomeComponent implements OnInit {
   itemCount: number = 4;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = ['do something cool', 'do something else cool', 'and another one'];
-  constructor() { }
+  goals = [];
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res)
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
 addItem() {
   this.goals.push(this.goalText);
   this.goalText = '';
   this.itemCount = this.goals.length;
+  this._data.changeGoal(this.goals);
 }
 
 removeItem(i) {
   this.goals.splice(i, 1)
+  this._data.changeGoal(this.goals);
 }
 
 }
